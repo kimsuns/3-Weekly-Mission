1721b125eb15b6afd4c6a47798b6811edbc0d7ca
kimsuns <kimksy27@gamil.com>
2023-12-17 01:01:12 +0900

    ADD PW AND LOGIN BOTTON
    비밀번호 경고창 저장 및 엔터키 입력시 로그인 기능 추가

--- signin.js
+++ signin.js
@@ -1,29 +1,61 @@
 
 
 const email = document.querySelector('#email');
+const pw = document.querySelector('#password');
+const btn = document.querySelector('.button-click');
 
 function inputEmail (e) {
   console.log(e.target.value);
-  if(e.target.value === ''){
+  if(email.value === ''){
     alert('이메일을 입력해주세요');
-  } else if(e.target.value.indexOf('@')=== -1){
+  } else if(email.value.indexOf('@')=== -1){
     alert('올바른 이메일 주소가 아닙니다.');
+  } else if(email.value !== 'test@codeit.com'){
+    alert('이메일을 확인해주세요.');
+  }
+}
+
+function inputPw (e) {
+  console.log(e.target.value);
+  if(pw.value === ''){
+    alert('비밀번호를 입력해주세요.');
+  } else if(pw.value !== 'codeit101'){
+    alert('비밀번호를 확인해주세요.');
+  }
+}
+
+function loginBtn () {
+  console.log()
+  if(email.value === 'test@codeit.com' &&  pw.value === 'codeit101') {
+    btn.innerHTML += '<a href="/folder.html">';
+    alert('로그인성공');
+
+  } else {
+    alert('다시 해보세요');
+  }
+}
+
+function loginKey (e) {
+  if(e.key === 'Enter') {
+    loginBtn();
   }
 }
 
 
 email.addEventListener('focusout', inputEmail);
+pw.addEventListener('focusout', inputPw);
+btn.addEventListener('click',loginBtn);
+btn.addEventListener('keypress', loginKey);
 
 
 /*
 4week
 
-
 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동합니다.
-비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해주세요.” 에러 메세지를 보입니다.
-이외의 로그인 시도의 경우, 이메일 input 아래에 “이메일을 확인해주세요.”, 비밀번호 input 아래에 “비밀번호를 확인해주세요.” 에러 메세지를 보입니다.
+
+
 input에 에러와 관련된 디자인은 좌측 상단의 Components 영역에 있는 디자인을 참고해 주세요.
-로그인 버튼 클릭 또는 Enter키 입력으로 로그인 실행돼야 합니다
+
 
 [심화 요구]
 눈 모양 아이콘 클릭시 비밀번호의 문자열이 보이기도 하고, 가려지기도 합니다.
@@ -34,4 +66,7 @@ input에 에러와 관련된 디자인은 좌측 상단의 Components 영역에 
 [완료]
 [v] 이메일 input에서 focus out 할 때, 값이 없을 경우 아래에 “이메일을 입력해주세요.” 에러 메세지를 보입니다.
 [v] 이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 아래에 “올바른 이메일 주소가 아닙니다.” 에러 메세지를 보입니다.
+[v] 비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해주세요.” 에러 메세지를 보입니다.
+[v] 이외의 로그인 시도의 경우, 이메일 input 아래에 “이메일을 확인해주세요.”, 비밀번호 input 아래에 “비밀번호를 확인해주세요.” 에러 메세지를 보입니다.
+[v] 로그인 버튼 클릭 또는 Enter키 입력으로 로그인 실행돼야 합니다
 */
