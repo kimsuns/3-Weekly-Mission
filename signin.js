cb503b705de1111006c8b266b336aff11a41b992
kimsuns <kimksy27@gamil.com>
2023-12-17 00:01:49 +0900

    ADD INCLUDE @ IN EMAIL
    이메일 형식 확인

--- signin.js
+++ signin.js
@@ -3,8 +3,11 @@
 const email = document.querySelector('#email');
 
 function inputEmail (e) {
-  if(e.key == null){
+  console.log(e.target.value);
+  if(e.target.value === ''){
     alert('이메일을 입력해주세요');
+  } else if(e.target.value.indexOf('@')=== -1){
+    alert('올바른 이메일 주소가 아닙니다.');
   }
 }
 
@@ -14,8 +17,8 @@ email.addEventListener('focusout', inputEmail);
 
 /*
 4week
-이메일 input에서 focus out 할 때, 값이 없을 경우 아래에 “이메일을 입력해주세요.” 에러 메세지를 보입니다.
-이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 아래에 “올바른 이메일 주소가 아닙니다.” 에러 메세지를 보입니다.
+
+
 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동합니다.
 비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해주세요.” 에러 메세지를 보입니다.
 이외의 로그인 시도의 경우, 이메일 input 아래에 “이메일을 확인해주세요.”, 비밀번호 input 아래에 “비밀번호를 확인해주세요.” 에러 메세지를 보입니다.
@@ -25,4 +28,10 @@ input에 에러와 관련된 디자인은 좌측 상단의 Components 영역에 
 [심화 요구]
 눈 모양 아이콘 클릭시 비밀번호의 문자열이 보이기도 하고, 가려지기도 합니다.
 비밀번호의 문자열이 가려질 때는 눈 모양 아이콘에는 사선이 그어져있고, 비밀번호의 문자열이 보일 때는 사선이 없는 눈 모양 아이콘이 보이도록 합니다
+*/
+
+/*
+[완료]
+[v] 이메일 input에서 focus out 할 때, 값이 없을 경우 아래에 “이메일을 입력해주세요.” 에러 메세지를 보입니다.
+[v] 이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 아래에 “올바른 이메일 주소가 아닙니다.” 에러 메세지를 보입니다.
 */
